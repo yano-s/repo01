87d8da8dabee8fc3735827daf6055af2e74c5f6e
z2g9112 <z2g9112@H1603027.intra.ctc-g.co.jp>
2017-08-28 14:18:17 +0900

    edit.

--- oshift-sample01/src/main/java/com/example/oshift/controller/HomeController.java
+++ oshift-sample01/src/main/java/com/example/oshift/controller/HomeController.java
@@ -3,6 +3,10 @@ package com.example.oshift.controller;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,17 +27,37 @@ public class HomeController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
+	public String home(Locale locale, HttpServletRequest request, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 
-//		Date date = new Date();
-//		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-//
-//		String formattedDate = dateFormat.format(date);
+		// Date date = new Date();
+		// DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG,
+		// DateFormat.LONG, locale);
+		//
+		// String formattedDate = dateFormat.format(date);
 
-		model.addAttribute("serverTime", DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss.SSS").format(LocalDateTime.now()) );
+		model.addAttribute("serverTime",
+				DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss.SSS").format(LocalDateTime.now()));
+		HttpSession session = request.getSession(true);
+		model.addAttribute("sessionId", session.getId());
+
+		AtomicInteger counter = (AtomicInteger) session.getAttribute("counter");
+		if (counter == null) {
+			counter = new AtomicInteger(0);
+			session.setAttribute("counter", counter);
+		}
+
+		model.addAttribute("counter", counter.incrementAndGet());
 
 		return "home";
+	}
+
+	/// clearCount
+	@RequestMapping(value = "/clearCount", method = RequestMethod.POST)
+	public String clearCount(Locale locale, HttpServletRequest request, Model model) {
+		HttpSession session = request.getSession();
+		session.invalidate();
+		return "redirect:/";
 	}
 
 }
--- oshift-sample01/src/main/webapp/WEB-INF/views/home.jsp
+++ oshift-sample01/src/main/webapp/WEB-INF/views/home.jsp
@@ -1,6 +1,9 @@
 <%@ page language="java" contentType="text/html; charset=utf-8"
-	pageEncoding="utf-8"%><%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
+	pageEncoding="utf-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="spring"%>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
+<%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions"%>
 <html>
 <head>
 	<title>Home</title>
@@ -9,9 +12,14 @@
 <h1>
 	初めてのOpenShift!
 </h1>
-<p>ひゃっはー！</p>
 <p>OpenShift起動確認！</p>
 <P>  The time on the server is ${serverTime}. </P>
+<P>  Session ID is ${sessionId}. </P>
+<P>  Page Count is ${counter}. </P>
+
+<form:form action="${pageContext.request.contextPath}/clearCount" method="POST">
+	<button type="submit">カウンタークリア</button>
+</form:form>
 
 </body>
 </html>
